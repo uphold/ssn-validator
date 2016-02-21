d5bdf1c572754ca6c46a270151c995e85637b41e
Rui Marinho <ruipmarinho@gmail.com>
2016-02-21 00:54:51 +0000

    Release 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,18 @@
 # Changelog
 
-## [0.0.2](https://github.com/seegno/is-valid-ssn/tree/0.0.2) (2016-02-17)
-[Full Changelog](https://github.com/seegno/is-valid-ssn/compare/0.0.1...0.0.2)
+## [1.0.0](https://github.com/seegno/ssn-validator/tree/1.0.0) (2016-02-21)
+[Full Changelog](https://github.com/seegno/ssn-validator/compare/0.0.2...1.0.0)
 
 **Merged pull requests:**
 
-- Update `es2015-node4` to `es2015` [\#3](https://github.com/seegno/is-valid-ssn/pull/3) ([ruipenso](https://github.com/ruipenso))
+- Add automated release process [\#4](https://github.com/seegno/ssn-validator/pull/4) ([ruimarinho](https://github.com/ruimarinho))
+- Add `strict` option [\#2](https://github.com/seegno/ssn-validator/pull/2) ([franciscocardoso](https://github.com/franciscocardoso))
 
-## [0.0.1](https://github.com/seegno/is-valid-ssn/tree/0.0.1) (2016-02-05)
+## [0.0.2](https://github.com/seegno/ssn-validator/tree/0.0.2) (2016-02-17)
+[Full Changelog](https://github.com/seegno/ssn-validator/compare/0.0.1...0.0.2)
+
+**Merged pull requests:**
+
+- Update `es2015-node4` to `es2015` [\#3](https://github.com/seegno/ssn-validator/pull/3) ([ruipenso](https://github.com/ruipenso))
+
+## [0.0.1](https://github.com/seegno/ssn-validator/tree/0.0.1) (2016-02-05)
--- dist/index.js
+++ dist/index.js
@@ -3,63 +3,62 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = undefined;
+exports.isValid = isValid;
 exports.mask = mask;
 
-var _lodash = require('lodash');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 /**
- * Blacklist.
- */
-
-var blacklist = ['078051120', '219099999', '457555462'];
-/**
- * See `http://www.irs.gov/Individuals/General-ITIN-Information` for more information.
- *
  * Social Security number (SSN) is a nine-digit number issued to U.S. citizens, permanent residents,
  * and * temporary (working) residents under section 205(c)(2) of the Social Security Act, codified
  * as 42 U.S.C. 405(c)(2).
+ *
+ * See `http://www.irs.gov/Individuals/General-ITIN-Information` for more information.
  */
 
 /**
- * Module dependencies.
+ * Blacklists.
  */
 
-var expression = /^(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}$/;
+var blacklist = ['078051120', '219099999', '457555462'];
+
+/**
+ * Expression.
+ */
+
+var expressions = {
+  soft: /^(?!666|000|9\d{2})\d{3}[- ]+?(?!00)\d{2}[- ]+?(?!0{4})\d{4}$/,
+  strict: /^(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}$/
+};
 
 /**
  * Validate function.
  */
 
 function isValid(ssn) {
-  if (!expression.test(ssn)) {
+  var _ref = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+  var _ref$strict = _ref.strict;
+  var strict = _ref$strict === undefined ? true : _ref$strict;
+
+  var mode = strict === true ? 'strict' : 'soft';
+
+  if (!expressions[mode].test(ssn)) {
     return false;
   }
 
-  return !_lodash2.default.includes(blacklist, ssn);
+  return blacklist.indexOf(ssn.replace(/\D/g, '')) === -1;
 }
 
 /**
  * Masks the SSN with "X" placeholders to protect sensitive data,
  * while keeping some of the original digits for contextual recognition.
  *
- * E.g. "123456789" -> "XXXXX6789"
+ * E.g. "123456789" -> "XXXXX6789", "123-45-6789" -> "XXX-XX-6789".
  */
 
-function mask(ssn) {
-  if (!isValid(ssn)) {
+function mask(ssn, options) {
+  if (!isValid(ssn, options)) {
     throw new Error('Invalid Social Security Number');
   }
 
-  return ssn.substr(0, ssn.length - 4).replace(/[\w]/g, 'X') + ssn.substr(-4);
+  return '' + ssn.substr(0, ssn.length - 4).replace(/[\w]/g, 'X') + ssn.substr(-4);
 }
-
-/**
- * Export default.
- */
-
-exports.default = isValid;
